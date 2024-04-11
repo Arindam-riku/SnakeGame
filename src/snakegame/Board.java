650c1d0a4ee3f165cb643fb7328956f2c73be246
Arindam Maji <118745079+arindam-maji@users.noreply.github.com>
2024-04-11 21:47:43 +0530

    Update Board.java

--- src/snakegame/Board.java
+++ src/snakegame/Board.java
@@ -13,9 +13,10 @@ public class Board extends JPanel implements ActionListener{//action listener ka
     private Image dot;
     private Image head;
     
-    private final int ALL_DOTS=900;//max size for dots
-    private final int DOT_SIZE=10;
-    private final int RANDOM_POSITION=29;
+    private final int ALL_DOTS=4900;//max size for dots
+    private final int DOT_SIZE=40;
+    private final int RANDOM_POSITION=17;
+    private int score=0;
     
     private int apple_x;
     private int apple_y;
@@ -35,11 +36,15 @@ public class Board extends JPanel implements ActionListener{//action listener ka
     Board(){
         addKeyListener(new TAdapter());//from action listener
         setBackground(Color.BLACK);
-        setPreferredSize(new Dimension(300,300));//(width,height)
+        setPreferredSize(new Dimension(700,700));//(width,height)
         setFocusable(true);//agar isko use nehi karte toh board start hone k baad hi game start ho jayega ,,to avoid this we use focusable()
         //now start the game
         loadImages();//actually the snake build on images,so we made this fucntion
         initGame();//game function
+        score();
+    }
+    public void score(){
+        
     }
     public void loadImages(){
         //iamgeicon is a class in jframe
@@ -52,11 +57,11 @@ public class Board extends JPanel implements ActionListener{//action listener ka
     }
     public void initGame(){
         for(int i=0;i<dots;i++){
-            y[i]=50;
-            x[i]=50-i*DOT_SIZE;
+            y[i]=350;
+            x[i]=350-i*DOT_SIZE;
         }
         locateApple();//create a function to initially place the apple
-        timer=new Timer(140,this);
+        timer=new Timer(200,this);
         timer.start();
     }
     
@@ -82,18 +87,26 @@ public class Board extends JPanel implements ActionListener{//action listener ka
                 g.drawImage(dot,x[i],y[i],this);
             }
         }
+
         Toolkit.getDefaultToolkit().sync();//to initialise default
         }else{
             gameOver(g);
+           // score(g);
         }
         
     }
-    public void gameOver(Graphics g){
-        String msg="!!Game Over!!";
-        Font font=new Font("SAN_SARIF",Font.BOLD,14);//in Font class Font(family,type,size)
-        FontMetrics metrices=getFontMetrics(font);//
+
+     
+     private void gameOver(Graphics g) {
+        String msg = "!!Game Over!!";
+        String s="    Score: "+score;
+        Font font = new Font("Helvetica", Font.BOLD, 40);
+        FontMetrics metr = getFontMetrics(font);
+
         g.setColor(Color.RED);
-        g.drawString(msg,(300-metrices.stringWidth(msg))/2,300/2);
+        g.setFont(font);
+        g.drawString(msg, (700 - metr.stringWidth(msg)) / 2,700/ 2);
+        g.drawString(s, (700 - metr.stringWidth(msg)) / 2,800/ 2);
     }
     public void move(){
         for(int i=dots;i>0;i--){
@@ -115,30 +128,40 @@ public class Board extends JPanel implements ActionListener{//action listener ka
         //x[0]+=DOT_SIZE;//only for head
        // y[0]+=DOT_SIZE;//only for head//IF WE icrease y also snake will go diagonaly
     }
-    public void eat(){
-        if((x[0]==apple_x)&&(y[0]==apple_y)){
-            dots++;
-            locateApple();
-        }
+//    public void eat(){
+//        if((x[0]==apple_x)&&(y[0]==apple_y)){
+//            dots++;
+//            locateApple();
+//        }
+//    }
+    public void eat() {
+    // Check if the snake's head position is close to the apple's position
+    if (x[0] >= apple_x - DOT_SIZE && x[0] <= apple_x + DOT_SIZE && 
+        y[0] >= apple_y - DOT_SIZE && y[0] <= apple_y + DOT_SIZE) {
+        dots++;
+        score++;
+        locateApple();
     }
+}
+
     public void collision(){
         for(int i=dots;i>0;i--){
             if((i>4)&&(x[0]==x[i])&&(y[0]==y[i])){
                 inGame=false;
             }
         }
-        if(y[0]>=300){
-            inGame=false;
-        }
-        if(x[0]>=300){
-            inGame=false;
-        }
-        if(y[0]<0){
-            inGame=false;
-        }
-        if(x[0]<0){
-            inGame=false;
-        }
+         if (y[0] >= getHeight()) {
+        inGame = false;
+    }
+    if (x[0] >= getWidth()) {
+        inGame = false;
+    }
+    if (y[0] < 0) {
+        inGame = false;
+    }
+    if (x[0] < 0) {
+        inGame = false;
+    }
         if(!inGame){
             timer.stop();
         }
